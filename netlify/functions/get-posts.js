efad762f83ade73d784fc8c6c69ef812f2a049da
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2021-12-09 08:39:26 +0100

    Set up headers

--- netlify/functions/get-posts.js
+++ netlify/functions/get-posts.js
@@ -1,22 +1,30 @@
 const axios = require('axios').default;
 
 exports.handler = async (event, context) => {
-    const data = await axios.get(`https://graph.instagram.com/me/media?access_token=${process.env.ACCESS_TOKEN}&limit=${process.env.LIMIT ? process.env.LIMIT : 9}&fields=media_type,media_url,timestamp,thumbnail_url`,
+    const response = {
+        headers: {
+            'Access-Control-Allow-Origin': `${process.env.ALLOWED_SITE || 'https://ppijerman.org'}`,
+            'Access-Control-Allow-Methods': 'GET',
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const data = await axios.get(`https://graph.instagram.com/me/media?access_token=${process.env.ACCESS_TOKEN}&limit=${process.env.LIMIT || 9}&fields=media_type,media_url,timestamp,thumbnail_url`,
         {
             method: `get`,
-            responseType: "json"
+            responseType: `json`
         }).then(response => response.data.data)
         .catch(error => console.error(error));
 
     if (data !== null) {
-        return {
-            statusCode: 200,
-            body: JSON.stringify(data)
-        }
+        response.statusCode = 200;
+        response.body = JSON.stringify({
+            data: data
+        });
     } else {
-        return {
-            statusCode: 500,
-            body: "Something bad happens"
-        }
+        response.statusCode = 500;
+        response.body = 'Something bad happens';
     }
+
+    return response;
 }
