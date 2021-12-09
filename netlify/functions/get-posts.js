b2eba9bc1c72802406f241316ab8f7d48006fd5d
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2021-12-09 01:19:34 +0100

    Using axios instead of fetch

--- netlify/functions/get-posts.js
+++ netlify/functions/get-posts.js
@@ -1,9 +1,22 @@
+const axios = require('axios').default;
+
 exports.handler = async (event, context) => {
-    return await fetch(`https://graph.instagram.com/me/media?access_token=${process.env.ACCESS_TOKEN}&limit=${process.env.LIMIT ? process.env.LIMIT : 9}&fields=media_type,media_url,timestamp,thumbnail_url`,
+    const data = await axios.get(`https://graph.instagram.com/me/media?access_token=${process.env.ACCESS_TOKEN}&limit=${process.env.LIMIT ? process.env.LIMIT : 9}&fields=media_type,media_url,timestamp,thumbnail_url`,
         {
-            method: `GET`,
-            mode: `cors`,
-            cache: `default`,
-        }).then(response => response.json())
+            method: `get`,
+            responseType: "json"
+        }).then(response => response.data)
         .catch(error => console.error(error));
+
+    if (data !== null) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data)
+        }
+    } else {
+        return {
+            statusCode: 500,
+            body: "Something bad happens"
+        }
+    }
 }
