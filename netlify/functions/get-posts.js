dfc0278816e4fa813df8aedab5a12517f519e1ff
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2022-01-08 14:08:20 +0100

    Fixed to working version for get-posts.js

--- netlify/functions/get-posts.js
+++ netlify/functions/get-posts.js
@@ -40,18 +40,19 @@ exports.handler = async (event, context) => {
 
     const tableName = process.env.TABLE_NAME;
 
-    return await mariadb.createConnection({
+    response.data = await mariadb.createConnection({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         port: process.env.DB_PORT,
         database: process.env.DB_NAME,
         password: process.env.DB_PASSWORD,
         compress: process.env.COMPRESSION_ENABLED === 'true',
-        connectTimeout: process.env.TIMEOUT || 2000
+        connectTimeout: process.env.TIMEOUT || 0
     }).then(conn => {
-        const res = conn.query(`SELECT *
-                                FROM ${tableName}
-                                WHERE tstamp = (SELECT MAX(tstamp) FROM ${tableName})`)
+
+        return conn.query(`SELECT *
+                           FROM ${tableName}
+                           WHERE tstamp = (SELECT MAX(tstamp) FROM ${tableName})`)
             .then(rows => {
                 console.log('Got data from database');
 
@@ -59,14 +60,13 @@ exports.handler = async (event, context) => {
                 const tstamp = rows.length > 0 ? new Date(rows[0]['tstamp']) : null;
                 const timeout = rows.length > 0 ? new Date(rows[0]['timeout']) : '0';
 
-                axios.get(`https://graph.instagram.com/me/media?access_token=${token}&limit=${process.env.LIMIT || 9}&fields=media_type,media_url,timestamp,thumbnail_url`, {
+                return axios.get(`https://graph.instagram.com/me/media?access_token=${token}&limit=${process.env.LIMIT || 9}&fields=media_type,media_url,timestamp,thumbnail_url,permalink`, {
                     method: `get`,
                     responseType: `json`
-                }).then(response => {
-                    const data = response.data.data
+                }).then(igRes => {
+                    const data = igRes.data.data
 
                     if (data !== null) {
-                        console.log('Got data: ', data);
                         response.statusCode = 200;
                         response.body = JSON.stringify({
                             data: data
@@ -88,17 +88,22 @@ exports.handler = async (event, context) => {
                         }).then(refreshResponse => {
                             // TODO check if refreshResponse is properly formatted, else cancel the operation in this part and print error to console
                             const encryptedNewToken = CryptoJS.AES.encrypt(refreshResponse.data['access_token'], process.env.PASSKEY);
+                            console.log(encryptedNewToken.toString().length);
                             try {
-                                conn.query(`INSERT INTO ${tableName} (access_token, timeout) VALUES (?, ?)`,
-                                    [encryptedNewToken.toString(), refreshResponse.data['expires_in']])
+                                conn.query(`INSERT INTO ${tableName} (access_tokens, timeout)
+                                            VALUES (?, ?)`,
+                                    ['test', refreshResponse.data['expires_in']])
                                     .then(res => {
                                         // TODO give output if the write operation is successful, else print error in console
                                     })
+
+                                conn.destroy();
                             } catch (e) {
                                 console.error('Got error when refreshing data in database with message: ', e);
                             }
                         }).catch(err => console.error(`Unable to get refresh access token! Error: ${err.toString()}`));
                     } else {
+                        conn.destroy();
                         console.warn(`Access token is not updated as the timestamp is not in the legal value (${tstamp.toLocaleString('de-DE')})`);
                     }
 
@@ -107,8 +112,7 @@ exports.handler = async (event, context) => {
 
             }).catch(err => this.constructResponse(response, 500, `Unable to get data from the database for getting token with message: ${err.toString()}`));
 
-        conn.destroy();
-
-        return res;
     }).catch(err => this.constructResponse(response, 500, `Cannot make connection with database server with message: ${err.toString()}`));
+
+    return response;
 }
